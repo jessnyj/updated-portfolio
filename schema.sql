c013769d1e67955e4f180d1fa34aab1284bc96e1
jessny <jessnyjoseph@gmail.com>
2021-02-15 17:41:34 -0600

    created schema

---
+++ schema.sql
@@ -0,0 +1,4 @@
+-- Drops the blogger if it exists currently --
+DROP DATABASE IF EXISTS portfolio_db;
+-- Creates the "blogger" database --
+CREATE DATABASE portfolio_db;
--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 const path = require("path");
-
 var express = require("express");
 
 var PORT = process.env.PORT || 3000;
 
+// Requiring models for syncing
 var db = require("./models");
 
 var app = express();
 
+// Sets up the Express app to handle data parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Static Directory
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
-
 // Routes
+require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
+
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function () {
     app.listen(PORT, function () {
