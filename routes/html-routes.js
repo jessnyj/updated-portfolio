cfdef61f4cfb13a95a6c7037afb2027ab6048a67
jessny <jessnyjoseph@gmail.com>
2021-02-15 21:28:22 -0600

    added html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,22 +10,21 @@ module.exports = function (app) {
 
     // index route loads index.html
     app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "./assets/index.html"));
+        res.sendFile(path.join(__dirname, "../public/assets/index.html"));
     });
-
     // projects route loads projects.html
     app.get("/projects", function (req, res) {
-        res.sendFile(path.join(__dirname, "./assets/projects.html"));
+        res.sendFile(path.join(__dirname,"../public/assets/projects.html"));
     });
 
     // about route loads about.html
     app.get("/about", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/about.html"));
+        res.sendFile(path.join(__dirname, "../public/assets/about.html"));
     });
 
     // contact route loads contact.html
     app.get("/contact", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/contact.html"));
+        res.sendFile(path.join(__dirname, "../public/assets/contact.html"));
     });
 
 };
